159c4dcb3f018e8765c0ace6fcfdc244075ffd72
Gaizka <gaizka.miranda@opendeusto.es>
2024-05-24 17:57:31 +0200

    Funcionalidad de mostrar u ocultar info (Gaizka)

---
+++ ProyectoWeb/static/js/mostrar_info.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+            const toggleButton = document.getElementById("btn-toggle");
+            const infoBlock = document.getElementById("info");
+
+            toggleButton.addEventListener("click", function() {
+                if (infoBlock.style.display === "none" || infoBlock.style.display === "") {
+                    infoBlock.style.display = "block";
+                    toggleButton.textContent = "Ocultar Información";
+                } else {
+                    infoBlock.style.display = "none";
+                    toggleButton.textContent = "Expandir Información";
+                }
+            });
+        });
