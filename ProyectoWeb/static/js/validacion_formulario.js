98a84f434e4ca58399a178ef995e5319635fc36e
Jon Ander <jonander.infante@opendeusto.es>
2024-05-26 23:26:15 +0200

    Prueba de soporte JS, visualizar y probar

--- ProyectoWeb/static/js/validacion_formulario.js
+++ ProyectoWeb/static/js/validacion_formulario.js
@@ -4,3 +4,37 @@ function validateForm() {
     var error = document.getElementById("error");
     error.innerHTML = "";
 }
+
+ //<script> PRUEBA PARA CAMBIAR SOLICITUD
+
+    const palabrasProhibidas = ["palabra1", "palabra2", "palabra3"];
+
+
+    function validarMensaje(event) {
+    const mensaje = event.target.value;
+    let mensajeModificado = mensaje;
+
+    palabrasProhibidas.forEach(palabra => {
+    const regex = new RegExp(`\\b(${palabra})\\b`, 'gi');
+    mensajeModificado = mensajeModificado.replace(regex, '<span class="highlight">$1</span>');
+});
+
+
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = mensajeModificado;
+
+    const textoPlano = tempDiv.textContent || tempDiv.innerText || "";
+
+
+    event.target.value = textoPlano;
+
+
+    document.getElementById('mensaje-preview').innerHTML = mensajeModificado;
+}
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.querySelector('textarea[name="mensaje"]');
+    textarea.addEventListener('input', validarMensaje);
+    })//;
+</script>
